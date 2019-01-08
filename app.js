405e1e4ff74d888201bb2d284291860c3ae32058
arthit75420 <arthit.n@ku.th>
2019-01-08 21:34:41 +0700

    tesa top gun 2019 group 30

---
+++ app.js
@@ -0,0 +1,39 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+var fs = require("fs");
+app.get('/',function(req,res){
+	res.send("Sample Code for RESTful API");
+});
+app.use(bodyParser.json());
+
+app.get('/listUsers', function (req, res) {
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+      console.log( data );
+      res.end( data );
+   });
+})
+
+var user = {
+   "user4" : {
+      "name" : "mohit",
+      "password" : "password4",
+      "profession" : "teacher",
+      "id": 4
+   }
+}
+
+app.post('/addUser', function (req, res) {
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+      data = JSON.parse( data );
+      data["user4"] = user["user4"];
+      console.log( data );
+      res.end( JSON.stringify(data));
+   });
+})
+
+var server = app.listen(8080, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
